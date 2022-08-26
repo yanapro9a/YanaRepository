f652c22a0ef8c5fbd1570f7ca34e6351f9918d43
Яна Пронина <yana@MacBook-Air-Ana.local>
2022-08-26 19:03:38 +0300

    Adding second task

---
+++ S2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Console. WriteLine ("Введите трехзначное число");
+int x = int. Parse (Console.ReadLine());
+int y =  (x%100)/10; 
+Console.WriteLine("Вторая цифра числа "  + x +  " равна " + y );*/
+
+/*;Console. WriteLine ("Введите целое натуральное число");
+int x = int.Parse(Console.ReadLine());
+if (x <=99) {
+    Console.WriteLine ("Третьей цифры нет!");
+}
+else {while (x>=1000) {
+    x = (x/10); }
+x = x%10; 
+Console.WriteLine("Третья цифра " + x);*/
+
+
+
+Console. WriteLine ("Введите число от 1 до 7");
+int x = int. Parse (Console.ReadLine());
+if (x == 7 || x ==6) {
+    Console.WriteLine("Ура!Выходной");
+}
+else if (x>7) 
+{
+
+    Console.WriteLine("Цифра должна быть меньше или равна 7");
+}
+else
+{
+    Console.WriteLine("Будний день - работаем!");}
+
+
+
