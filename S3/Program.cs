beeacd187ca45d40ea9d93781123f4644ec6e46a
Яна Пронина <yana@MacBook-Air-Ana.local>
2022-08-28 11:43:59 +0300

    seminar 3

---
+++ S3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Console.WriteLine("Введите пятизначное число");
+int x = int.Parse(Console.ReadLine());
+int first = x/10000;
+int second = (x/1000)%10;
+int fourth = (x/10)%10;
+int fifth= x%10;
+
+if(x<=9999 || x>99999)
+    Console.WriteLine("Число не пятизначное. Введите пятизначное");
+else if (first == fifth && second == fourth)
+    Console.WriteLine("Число - палиндром");
+else
+    Console.WriteLine("Число НЕ палиндром");*/
+
+/*Console.WriteLine("Введите координнату x первой точки");
+int x1 = int. Parse (Console.ReadLine());
+Console.WriteLine("Введите координнату y первой точки");
+int y1 = int. Parse (Console.ReadLine());
+Console.WriteLine("Введите координнату z первой точки");
+int z1 = int. Parse (Console.ReadLine());
+
+Console.WriteLine("Введите координнату x второй точки");
+int x2 = int. Parse (Console.ReadLine());
+Console.WriteLine("Введите координнату y второй точки");
+int y2 = int. Parse (Console.ReadLine());
+Console.WriteLine("Введите координнату z второй точки");
+int z2 = int. Parse (Console.ReadLine());
+
+double rast = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+Console.WriteLine(rast);*/
+
+
+/*Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+if (n==0) {
+     Console.WriteLine("Не могу посчитать, число равно 0");
+}
+else {
+    Console.WriteLine("Таблица кубов:");
+    for (int i=1;i<=n;i++){
+    Console.WriteLine(Math.Pow(i,3));
+    }
+}*/
+
+
