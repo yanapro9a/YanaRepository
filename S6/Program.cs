a8bd21319bf9d4ecd3fdfbe17d8824828f4b7eeb
Яна Пронина <yana@MacBook-Air-Ana.local>
2022-09-10 18:11:20 +0300

    Adding tasks for seminar 6

---
+++ S6/Program.cs
@@ -0,0 +1,32 @@
+﻿/*void FillArray(int[]array) //задача 1
+{
+for (int i=0; i<array.Length;i++) {
+     Console.WriteLine("Введите число");
+    array[i]= int. Parse(Console.ReadLine());}
+};
+
+void Poisk(int[]array) {
+    int counter = 0;
+    for (int i=0;i<array.Length;i++) {
+    if (array[i]>0) counter++; 
+    }
+    Console.WriteLine("Количество чисел больше 0 равно " + counter);
+
+}
+
+int [] array = new int[5];
+FillArray(array);
+Poisk(array); */
+
+/*Console.WriteLine("Введите число b1");//задача 2
+double b1 = int. Parse(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+double k1 = int. Parse(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+double b2 = int. Parse(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2= int. Parse(Console.ReadLine());
+
+double coor1=-(b1-b2)/(k1-k2);
+double coor2=k2*coor1+b2;
+Console.WriteLine("Координаты точки пересечения " + coor1 + "  " + coor2);*/
